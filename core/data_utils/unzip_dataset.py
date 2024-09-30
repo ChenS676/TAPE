5a5f46e1a49d95197778a5d9368f33266eefc720
cc7738@kit.edu <cc7738@uc2n564.localdomain>
2024-10-01 00:34:10 +0200

    unzip

--- core/data_utils/unzip_dataset.py
+++ core/data_utils/unzip_dataset.py
@@ -2,8 +2,15 @@ import os, sys
 import tarfile
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-file_path = "dataset.tar.gz"
-extract_path = "./core"
-with tarfile.open(file_path, "r:gz") as tar:
-    tar.extractall(path=extract_path)
-    print(f"Extracted all contents to {extract_path}.")
+# file_path = "dataset.tar.gz"
+# extract_path = "./core"
+# with tarfile.open(file_path, "r:gz") as tar:
+#     tar.extractall(path=extract_path)
+#     print(f"Extracted all contents to {extract_path}.")
+    
+import psutil
+
+def print_cpu_memory():
+    process = psutil.Process(os.getpid())
+    mem_info = process.memory_info()
+    print(f"RSS Memory: {mem_info.rss / 1e6} MB")
